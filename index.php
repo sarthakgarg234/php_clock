8e85ac9005f00e8784088267c888839bf4dd9490
sarthakgarg234 <sarthakgarg234@gmail.com>
2021-04-05 21:23:16 +0530

    New changes

--- index.php
+++ index.php
@@ -4,4 +4,5 @@
     echo "Today is " . date("Y-m-d") . "<br>";
     echo "Today is " . date("l") . "<br>";
     echo "Today is " . date("Y") . "<br>";
+    echo "Today is " . date("Y") . "<br>";
 ?>
