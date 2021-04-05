9a86f9efaaf23d654b03fb474cff6e4434171f6b
sarthakgarg234 <81975443+sarthakgarg234@users.noreply.github.com>
2021-04-05 21:30:55 +0530

    Update index.php
    I have changed something

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
     echo "Today is " . date("l") . "<br>";
     echo "Today is " . date("Y") . "<br>";
     echo "Today is " . date("Y") . "<br>";
+    echo "Directly github changes " . date("Y") . "<br>";
 ?>
