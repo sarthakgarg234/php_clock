51b0f92f30fec0313f0c1f247ff1b3bc80bbdb7d
sarthakgarg234 <sarthakgarg234@gmail.com>
2021-04-06 20:30:49 +0530

    New Changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     echo "Today is " . date("l") . "<br>";
     echo "Today is " . date("Y") . "<br>";
     echo "Today is " . date("Y") . "<br>";
+    echo "Today is " . date("Y") . "<br>";
     echo "Directly github changes " . date("Y") . "<br>";
     echo "Directly github changes " . date("Y") . "<br>";
 ?>
